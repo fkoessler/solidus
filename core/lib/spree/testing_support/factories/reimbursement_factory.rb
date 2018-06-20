f3f46f1a17086b125a25fa685b2b2dc5d74bd572
François Koessler <francois@khawa.tech>
2018-06-20 10:14:30 +0200

    Refactor settlement factory
    Use the same association logic as return_items

--- core/lib/spree/testing_support/factories/reimbursement_factory.rb
+++ core/lib/spree/testing_support/factories/reimbursement_factory.rb
@@ -1,9 +1,11 @@
 require 'spree/testing_support/factories/customer_return_factory'
+require 'spree/testing_support/factories/settlement_factory'
 
 FactoryGirl.define do
   factory :reimbursement, class: 'Spree::Reimbursement' do
     transient do
       return_items_count 1
+      settlements_count 0
     end
 
     customer_return { create(:customer_return_with_accepted_items, line_items_count: return_items_count) }
@@ -14,6 +16,13 @@ FactoryGirl.define do
         reimbursement.return_items = reimbursement.customer_return.return_items
       end
       reimbursement.total = reimbursement.return_items.map { |ri| ri.amount }.sum
+    end
+
+    after(:create) do |reimbursement, evaluator|
+      if evaluator.settlements_count > 0
+        shipment = reimbursement.return_items.first.shipment
+        create_list(:settlement, evaluator.settlements_count, reimbursement: reimbursement, shipment: shipment)
+      end
     end
   end
 end
